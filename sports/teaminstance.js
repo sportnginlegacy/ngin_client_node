e3c8e4764fb8c4d694e4e9f3e757f1392fc7d0f1
Darcy Murphy <mrDarcyMurphy@gmail.com>
2012-10-29 16:50:46 -0500

    updated client to use subseasons to fetch team instances

--- sports/teaminstance.js
+++ sports/teaminstance.js
@@ -33,14 +33,14 @@ var TeamInstance = Model.extend({
   url: function(options){
     // TODO: throw an error if no season_id?
     var base = config.urls && config.urls.sports || config.url
-    base = Url.resolve(base, '/seasons/' + options.season_id + '/teams' )
+    base = Url.resolve(base, '/subseasons/' + options.subseason_id + '/teams' )
     if (!this.id) return base
     return base + (base.charAt(base.length - 1) === '/' ? '' : '/') + encodeURIComponent(this.id)
   },
 
   urlRoot: function() {
     var base = config.urls && config.urls.sports || config.url
-    return Url.resolve(base, '/seasons')
+    return Url.resolve(base, '/subseasons')
   },
 
   initialize: function(attr, options) {
