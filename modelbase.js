96e9f8ea678c8c2f71433b76d58100d3182e3f27
Darcy Murphy <mrDarcyMurphy@gmail.com>
2012-10-29 15:29:50 -0500

    passing options to url method

--- modelbase.js
+++ modelbase.js
@@ -111,7 +111,9 @@ _.extend(Model, {
     var self = this
     options || (options = {})
 
-    if (!options.url) options.url = _.result(this.prototype, 'url')
+    if (!options.url) {
+      options.url = _.isFunction(this.prototype.url) ? this.prototype.url(options) : this.prototype.url
+    }
 
     sync('read', null, options, function(err, data, resp) {
       if (err) return callback(err)
