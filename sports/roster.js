be0458d0e42a24f046069ddbe14a2bc3a9cc5d5d
Darcy Murphy <mrDarcyMurphy@gmail.com>
2012-10-30 16:37:47 -0500

    updated roster model url method

--- sports/roster.js
+++ sports/roster.js
@@ -30,6 +30,14 @@ function init(conf) {
 
 var Roster = Model.extend({
 
+  url: function(options){
+    // TODO: throw an error if no season_id or team_id
+    var base = config.urls && config.urls.sports || config.url
+    base = Url.resolve(base, '/subseasons/' + options.subseason_id + '/teams/' + options.team_id + '/rosters' )
+    if (!this.id) return base
+    return base + (base.charAt(base.length - 1) === '/' ? '' : '/') + encodeURIComponent(this.id)
+  },
+
   urlRoot: function() {
     var base = config.urls && config.urls.sports || config.url
     return Url.resolve(base, '/rosters')
