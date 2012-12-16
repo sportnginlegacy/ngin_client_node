2f65d4a43c3da14867c9d89b93de2e045b8a81ff
Jesse Houchins <jesse.houchins@tstmedia.com>
2012-12-15 18:20:08 -0600

    add/remove method on flight stage

--- sports/flightstage.js
+++ sports/flightstage.js
@@ -21,6 +21,16 @@ module.exports = function(ngin) {
 
     validate: function() {
       return ~['pool', 'bracket'].indexOf(this.type) ? false : 'Property "type" has an invalid value'
+    },
+
+    addTeam: function(teamID, callback) {
+      var url = this.urlRoot() + '/' + this.id + '/add_team/' + teamID
+      FlightStage.sync('update', null, { url:url }, callback)
+    },
+
+    removeTeam: function(teamID, callback) {
+      var url = this.urlRoot() + '/' + this.id + '/remove_team/' + teamID
+      FlightStage.sync('delete', null, { url:url }, callback)
     }
 
   })
