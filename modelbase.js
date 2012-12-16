cceb8d88eab29b1f65be27a1b8ef520f19dd9a3d
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2012-12-16 17:01:05 -0600

    rejecting id when overriding create

--- modelbase.js
+++ modelbase.js
@@ -70,7 +70,7 @@ module.exports = function(ngin) {
       // Get base url
       var url = (this.urlRoot instanceof Function) ? this.urlRoot(options) : this.urlRoot
       // Append id if set
-      if (this.id) url += (url.charAt(url.length - 1) === '/' ? '' : '/') + encodeURIComponent(this.id)
+      if (this.id && !this.createOverride) url += (url.charAt(url.length - 1) === '/' ? '' : '/') + encodeURIComponent(this.id)
       // Add options as query parameters
       var separator = "?"
       _.each(options, function(val, key){
