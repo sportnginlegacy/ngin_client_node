cd878cc3010697e83a55d4740177e48cf6477826
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2012-12-29 15:10:09 -0600

    modified root url to reflect object_id and added publish method

--- sports/tournamentSchedule.js
+++ sports/tournamentSchedule.js
@@ -16,9 +16,31 @@ module.exports = function(ngin) {
 
   var TournamentSchedule = SportsModel.extend({
 
-    urlRoot: function() {
+    /**
+    * create: flight_id
+    * publish: flight_id
+    * update: game_slot_id
+    */
+
+    urlRoot: function(options) {
+      options = options || {}
+      var objectID = null
+      if (options.flight_id || this.flight_id) {
+        objectID = options.flight_id || this.flight_id
+        this.object_id = flightID
+        delete options.flight_id
+      } else if (options.game_slot_id || this.game_slot_id) {
+        objectID = options.game_slot_id || this.game_slot_id
+        this.object_id = objectID
+        delete options.game_slot_id
+      }
       var base = config.urls && config.urls.sports || config.url
-      return Url.resolve(base, '/tournament_schedules')
+      return Url.resolve(base, 'tournament_schedules/' + objectID)
+    },
+
+    publish:function(options, callback) {
+      var url = this.urlRoot() + '/publish'
+      TournamentSchedule.sync('create', null, { url:url }, callback)
     }
   })
 
