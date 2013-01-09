2a5237ee2c3fb9a8f5d0c0def12eb111bea4715e
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2013-01-09 10:26:56 -0600

    added endpoint for tiebreak criteria

---
+++ sports/tiebreakCriteria.js
@@ -0,0 +1,28 @@
+var Url = require('url')
+var _ = require('underscore')
+
+module.exports = function(ngin) {
+  var SportsModel = ngin.SportsModel
+  var config = ngin.config
+
+  /**
+   * StandingsPreference Class
+   *
+   * @param {Object} attr
+   * @param {Object} options
+   * @api public
+   */
+
+  var TiebreakCriteria = SportsModel.extend({
+
+    urlRoot: function(options) {
+      var base = config.urls && config.urls.sports || config.url
+      return Url.resolve(base, scope + '/tiebreak_criteria')
+    }
+
+  })
+
+
+  return TiebreakCriteria
+
+}
