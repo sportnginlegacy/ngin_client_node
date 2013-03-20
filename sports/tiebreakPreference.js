316b801335824361182adbd85033a2f0781fa256
Mike Frey <mike.frey@tstmedia.com>
2013-03-20 17:16:23 -0500

    updated tiebreakPreference model

--- sports/tiebreakPreference.js
+++ sports/tiebreakPreference.js
@@ -5,6 +5,30 @@ var _ = require('underscore')
 module.exports = function(ngin) {
   var SportsModel = ngin.SportsModel
   var config = ngin.config
+  var Super = SportsModel.prototype
+
+
+  /**
+   * Scopes the url to the tournament or flight
+   *
+   * @param {Object} options
+   * @returns {String}
+   * @api public
+   */
+
+  function scopeUrl(options, inst) {
+    options = _.extend(_.clone(options || {}), inst)
+    if (typeof options !== 'object' && (!options.tournament_id || !options.flight_id))
+      throw new Error('tournament_id or flight_id required to make tibreak preference api calls')
+
+    var url = ''
+    if (options.tournament_id) {
+      url += ngin.Tournament.urlRoot() + '/' + options.tournament_id
+    } else if (options.flight_id) {
+      url += ngin.Flight.urlRoot() + '/' + options.flight_id
+    }
+    return url + TiebreakPreference.urlRoot()
+  }
 
   /**
    * TiebreakPreference Class
@@ -16,45 +40,31 @@ module.exports = function(ngin) {
 
   var TiebreakPreference = SportsModel.extend({
 
-    urlRoot: function(options) {
-      options = options || {}
-      var scope = ''
-      if (options.tournament_id || this.tournament_id) {
-        var tournamentID = options.tournament_id || this.tournament_id
-        this.tournament_id = tournamentID
-        scope = 'tournaments/' + tournamentID
-        delete options.tournament_id
-      } else if (options.flight_id || this.flight_id) {
-        var flightID = options.flight_id || this.flight_id
-        this.flight_id = flightID
-        scope = 'flights/' + flightID
-        delete options.flight_id
-      }
-
-      var base = config.urls && config.urls.sports || config.url
-      return Url.resolve(base, scope + '/tiebreak_preference')
+    // Fetch is used by Tournament and
+    // list is being used by Flight. Can those
+    // be consolidated?
+    fetch: function(options, callback) {
+      var url = scopeUrl(options, this)
+      Super.fetch.call(this, url, options, callback)
     },
 
     save: function(options, callback) {
-      if (typeof options == 'function') {
-        callback = options
-        options = {}
-      }
-
+      var url = scopeUrl(options, this)
       options.method = options.method || 'PUT'
-      TiebreakPreference.__super__.save.call(this, options, callback)
+      Super.save.call(this, url, options, callback)
+    }
+
+  }, {
+
+    urlRoot: function() {
+      // No need for the base portion of the url here
+      // since this url must be scoped to another object.
+      return '/tiebreak_preference'
     },
 
-    url: function(options){
-      // Get base url
-      var url = (this.urlRoot instanceof Function) ? this.urlRoot(options) : this.urlRoot
-      // Add options as query parameters
-      var separator = "?"
-      _.each(options, function(val, key){
-        url += separator + encodeURIComponent(key) + "=" + encodeURIComponent(val)
-        separator = "&"
-      })
-      return url
+    list: function(options, callback) {
+      var url = scopeUrl(options)
+      SportsModel.list.call(this, url, options, callback)
     }
 
   })
