2233eb4183337b3ec2d5195afecdf782caffb268
Kevin Marx <kevin.marx@tstmedia.com>
2013-04-16 14:24:27 -0500

    this needs to be id otherwise fetch won't be called

--- src/sports/teamInstance.js
+++ src/sports/teamInstance.js
@@ -17,9 +17,9 @@ module.exports = function(ngin) {
 
   function scopeUrl(options, inst) {
     options = _.extend(_.clone(options || {}), inst)
-    if (!options.subseason_id || !options.team_id)
+    if (!options.subseason_id || !options.id)
       throw new Error('subseason_id and team_id required to make team instance api calls')
-    return ngin.Subseason.urlRoot() + '/' + options.subseason_id + '/teams/' + options.team_id
+    return ngin.Subseason.urlRoot() + '/' + options.subseason_id + '/teams/' + options.id
   }
 
   /**
