b7b862dc9c2584988d8d1178eefc5e2cdb937bb3
Mike Frey <mike.frey@tstmedia.com>
2013-04-23 16:03:27 -0500

    added list method for role

--- src/users/role.js
+++ src/users/role.js
@@ -21,6 +21,11 @@ module.exports = function(ngin) {
     urlRoot: function() {
       var base = config.urls && config.urls.users || config.url
       return Url.resolve(base, '/roles')
+    },
+
+    list: function(options, callback) {
+      var url = Role.urlRoot()
+      return Model.list.call(this, url, options, callback)
     }
 
   })
