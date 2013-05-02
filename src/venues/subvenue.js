583307b479a63b8096b706c729a6b27a06255388
Kevin Marx <kevin.marx@tstmedia.com>
2013-05-02 13:52:48 -0500

    moved scope url out so we have access to it when scoping list urls

--- src/venues/subvenue.js
+++ src/venues/subvenue.js
@@ -7,20 +7,6 @@ module.exports = function(ngin) {
   var Super = Model.prototype
   var config = ngin.config
 
-  /**
-   * Scopes the url to the tournament or flight
-   *
-   * @param {Object} options
-   * @returns {String}
-   * @api public
-   */
-
-  function scopeUrl(options, inst) {
-    options = _.extend(_.clone(options || {}), inst)
-    if (!options.venue_id)
-      throw new Error('venue_id required to make subvenue api calls')
-    return ngin.Venue.urlRoot() + '/' + options.venue_id + '/subvenues'
-  }
 
   /**
    * Subvenue Class
@@ -33,17 +19,17 @@ module.exports = function(ngin) {
   var Subvenue = Model.extend({
 
     fetch: function(options, callback) {
-      var url = scopeUrl(options, this) + '/' + this.id
+      var url = Subvenue.scopeUrl(options, this) + '/' + this.id
       return Super.fetch.call(this, url, options, callback)
     },
 
     save: function(options, callback) {
-      var url = scopeUrl(options, this) + (this.id ? '/' + this.id : '')
+      var url = Subvenue.scopeUrl(options, this) + (this.id ? '/' + this.id : '')
       return Super.save.call(this, url, options, callback)
     },
 
     destroy: function(options, callback) {
-      var url = scopeUrl(options, this) + '/' + this.id
+      var url = Subvenue.scopeUrl(options, this) + '/' + this.id
       return Super.destroy.call(this, url, options, callback)
     },
 
@@ -59,13 +45,30 @@ module.exports = function(ngin) {
 
   }, {
 
+  /**
+   * Scopes the url to the tournament or flight
+   *
+   * @param {Object} options
+   * @returns {String}
+   * @api public
+   */
+
+    scopeUrl: function(options, inst) {
+      options = _.extend(_.clone(options || {}), inst)
+      if (!options.venue_id)
+        throw new Error('venue_id required to make subvenue api calls')
+      return ngin.Venue.urlRoot() + '/' + options.venue_id + '/subvenues'
+    },
+
     urlRoot: function(options) {
       var base = config.urls && config.urls.venues || config.url
       return Url.resolve(base, '/subvenues')
     },
 
     list: function(options, callback) {
+      console.log("OPTIONS", options)
       var url = Subvenue.urlRoot()
+      if (options.venue_id && !_.isEmpty(options.venue_id)) url = Subvenue.scopeUrl(options, this)
       return Model.list.call(Subvenue, url, options, callback)
     }
 
