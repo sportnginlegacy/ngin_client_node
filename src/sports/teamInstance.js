9792f154eae6ee27d719523d2df3f2a1ae29ac3d
Kevin Marx <kevin.marx@tstmedia.com>
2013-07-01 15:45:10 -0500

    Support both season_id and subseason_id for now

--- src/sports/teamInstance.js
+++ src/sports/teamInstance.js
@@ -17,9 +17,12 @@ module.exports = function(ngin) {
 
   function scopeUrl(options, inst) {
     options = _.extend(_.clone(options || {}), inst)
-    if (!options.season_id || !options.team_id)
-      throw new Error('season_id and team_id required to make team instance api calls')
-    return ngin.Season.urlRoot() + '/' + options.season_id + '/teams/' + options.team_id
+    if (!options.season_id && !options.subseason_id || !options.team_id)
+      throw new Error('season_id or subseason_id and team_id required to make team instance api calls')
+    if (options.season_id)
+      return ngin.Season.urlRoot() + '/' + options.season_id + '/teams/' + options.team_id
+    else
+      return ngin.Subseason.urlRoot() + '/' + options.subseason_id + '/teams/' + options.team_id
   }
 
   /**
@@ -56,12 +59,14 @@ module.exports = function(ngin) {
     },
 
     list: function(options, callback) {
-      if (!options.season_id && !options.team_id)
-        throw new Error('season_id or team_id required to list team instances')
+      if (!options.season_id && !options.subseason_id && !options.team_id)
+        throw new Error('season_id or subseason_id or team_id required to list team instances')
 
       var url
       if (options.season_id)
         url = ngin.Season.urlRoot() + '/' + options.season_id + '/teams'
+      if (options.subseason_id)
+        url = ngin.Subseason.urlRoot() + '/' + options.subseason_id + '/teams'
       if (options.team_id)
         url = ngin.Team.urlRoot() + '/' + options.team_id + '/team_instances'
 
