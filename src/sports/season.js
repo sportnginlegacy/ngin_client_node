5b2ec03601a8336b0dda458c14864c356f373c97
Jesse Houchins <jesse.houchins@tstmedia.com>
2013-09-10 15:58:28 -0500

    add season player methods

--- src/sports/season.js
+++ src/sports/season.js
@@ -46,6 +46,24 @@ module.exports = function(ngin) {
       return ngin.TeamInstance.list({season_id: this.id}, callback)
     },
 
+    addPlayer: function(playerID, callback) {
+      var url = Season.urlRoot() + '/' + this.id + '/players?player_id=' + playerID
+      return Season.sync('update', null, { url:url }, this.callbackWithParse(callback))
+    },
+
+    removePlayer: function(playerID, callback) {
+      var url = Season.urlRoot() + '/' + this.id + '/players/' + playerID
+      return Season.sync('delete', null, { url:url }, callback)
+    },
+
+    players: function(options, callback) {
+      if (typeof options == 'function') {
+        callback = options, options = {}
+      }
+      options.url = Season.urlRoot() + '/' + this.id + '/players'
+      return ngin.Player.list(options, callback)
+    },
+
     divisions: function(callback) {
       return ngin.Division.list({season_id: this.id}, callback)
     },
