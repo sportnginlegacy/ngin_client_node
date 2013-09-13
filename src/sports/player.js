71e753edf7cdecacb0392de62839f73cadb7e7c0
Jesse Houchins <jesse.houchins@tstmedia.com>
2013-09-13 13:20:04 -0500

    revert to master players

--- src/sports/player.js
+++ src/sports/player.js
@@ -18,29 +18,29 @@ module.exports = function(ngin) {
   var Player = SportsModel.extend({
 
     fetch: function(options, callback) {
-      var url = ngin.Player.urlRoot() + '/' + this.id
+      var url = Player.urlRoot() + '/' + this.id
       return Super.fetch.call(this, url, options, callback)
     },
 
     save: function(options, callback) {
-      var url = ngin.Player.urlRoot() + (this.id ? '/' + this.id : '')
+      var url = Player.urlRoot() + (this.id ? '/' + this.id : '')
       return Super.save.call(this, url, options, callback)
     },
 
     destroy: function(options, callback) {
-      var url = ngin.Player.urlRoot() + '/' + this.id
+      var url = Player.urlRoot() + '/' + this.id
       return Super.destroy.call(this, url, options, callback)
     }
 
   },{
 
-    urlRoot: function(opts) {
+    urlRoot: function() {
       var base = config.urls && config.urls.sports || config.url
       return Url.resolve(base, '/players')
     },
 
     list: function(options, callback) {
-      var url = ngin.Player.urlRoot()
+      var url = Player.urlRoot()
       return SportsModel.list.call(this, url, options, callback)
     }
 
