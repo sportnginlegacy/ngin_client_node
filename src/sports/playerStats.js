3b0d457c17d0ee6ceebac1d74d3322ce2319568e
Mike Frey <mike.frey@sportngin.com>
2014-11-18 08:07:08 -0600

    player stats

---
+++ src/sports/playerStats.js
@@ -0,0 +1,43 @@
+"use strict"
+var Url = require('url')
+var _ = require('underscore')
+
+module.exports = function(ngin) {
+  var SportsModel = ngin.SportsModel
+  var Super = SportsModel.prototype
+  var config = ngin.config
+
+  var PlayerStats = SportsModel.extend({
+
+    fetch: function(options, callback) {
+      var url = PlayerStats.urlRoot() + '/' + this.id
+      return Super.fetch.call(this, url, options, callback)
+    },
+
+    save: function(options, callback) {
+      var url = PlayerStats.urlRoot() + (this.id ? '/' + this.id : '')
+      return Super.save.call(this, url, options, callback)
+    },
+
+    destroy: function(options, callback) {
+      var url = PlayerStats.urlRoot() + '/' + this.id
+      return Super.destroy.call(this, url, options, callback)
+    }
+
+  }, {
+
+    urlRoot: function(options) {
+      var base = config.urls && config.urls.sports || config.url
+      return Url.resolve(base, '/player_stats')
+    },
+
+    list: function(options, callback) {
+      var url = PlayerStats.urlRoot()
+      return SportsModel.list.call(this, url, options, callback)
+    }
+
+  })
+
+  return PlayerStats
+
+}
---
+++ test/sports/playerStats_test.js
@@ -0,0 +1,88 @@
+"use strict"
+var assert = require('assert')
+var sinon = require('sinon')
+
+var Server = require('../fixtures/http.js')
+var NginClient = require('../../index')
+var ngin = new NginClient(require('../fixtures/config.js'))
+
+var server
+
+describe('PlayerStats Model', function() {
+
+  before(function(done) {
+    server = Server(done)
+  })
+
+  after(function(done) {
+    server.close(done)
+  })
+
+  describe('PlayerStats Class', function() {
+
+    it('should make requests on create with ID', function(done) {
+      ngin.PlayerStats.create({id:1}, function(err, result, data, resp) {
+        console.log(err)
+        assert(!err)
+        assert(!!result)
+        assert.equal(resp.req.method, 'GET')
+        assert.equal(resp.req.path, '/player_stats/1')
+        done()
+      })
+    })
+
+    it('should make requests on list', function(done) {
+      ngin.PlayerStats.list(function(err, data, resp) {
+        assert(!err)
+        assert(!!resp)
+        assert.equal(resp.req.method, 'GET')
+        assert.equal(resp.req.path, '/player_stats')
+        done()
+      })
+    })
+
+  })
+
+  describe('PlayerStats Instance', function() {
+
+    var obj
+
+    beforeEach(function() {
+      obj = ngin.PlayerStats.create({id:1}, {fetched:true})
+    })
+
+    it('should make requests on save with ID', function(done) {
+      obj.save(function(err, data, resp) {
+        assert(!err)
+        assert(!!resp)
+        assert.equal(resp.req.method, 'PUT')
+        assert.equal(resp.req.path, '/player_stats/1')
+        done()
+      })
+    })
+
+    it('should make requests on save without ID', function(done) {
+      delete obj.id
+      obj.save(function(err, data, resp) {
+        assert(!err)
+        assert(!!resp)
+        assert.equal(resp.req.method, 'POST')
+        assert.equal(resp.req.path, '/player_stats')
+        done()
+      })
+    })
+
+    it('should make requests on destroy with ID', function(done) {
+      obj.destroy(function(err, data, resp) {
+        assert(!err)
+        assert(!!resp)
+        assert.equal(resp.req.method, 'DELETE')
+        assert.equal(resp.req.path, '/player_stats/1')
+        done()
+      })
+    })
+
+  })
+
+
+})
