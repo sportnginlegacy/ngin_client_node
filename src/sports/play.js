54f862417d809110f6e4e00161e4f490bd33204a
Bob Black <bob.black@sportngin.com>
2015-02-10 17:18:30 -0600

    Add sports/play

---
+++ src/sports/play.js
@@ -0,0 +1,37 @@
+"use strict"
+
+module.exports = function(ngin) {
+  var SportsModel = ngin.SportsModel
+  var Super = SportsModel.prototype
+  var config = ngin.config
+
+  /**
+   * Play Class
+   *
+   * @param {Object} attr
+   * @param {Object} options
+   * @api public
+   */
+
+  var Play = SportsModel.extend({
+
+    fetch: function(options, callback) {
+      var url = Play.urlRoot() + '/' + this.id
+      return Super.fetch.call(this, url, options, callback)
+    }
+
+  }, {
+
+    urlRoot: function() {
+      return this.resolve('/plays')
+    },
+
+    list: function(options, callback) {
+      var url = Play.urlRoot()
+      return SportsModel.list.call(this, url, options, callback)
+    }
+
+  })
+
+  return Play
+}
