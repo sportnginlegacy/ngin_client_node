0099ebb6d4129e3ab8beb5eb5756ae6992b8f751
Bob Black <bob.black@sportngin.com>
2015-02-11 11:58:00 -0600

    Add PlayAction

---
+++ src/sports/playAction.js
@@ -0,0 +1,41 @@
+"use strict"
+
+module.exports = function(ngin) {
+  var SportsModel = ngin.SportsModel
+  var Super = SportsModel.prototype
+  var config = ngin.config
+
+  /**
+   * PlayAction Class
+   *
+   * @param {Object} attr
+   * @param {Object} options
+   * @api public
+   */
+
+  var PlayAction = SportsModel.extend({
+
+    fetch: function(options, callback) {
+      return Super.fetch.call(this, this.urlById(), options, callback)
+    }
+
+  }, {
+
+    urlRoot: function() {
+      return this.resolve('/play_actions')
+    },
+
+    list: function(options, callback) {
+      if (!options.play_id)
+        return callback(new Error('play_id is required to list plays'))
+      options.query = options.query || {}
+      options.query.play_id = options.play_id
+
+      var url = PlayAction.urlRoot()
+      return SportsModel.list.call(this, url, options, callback)
+    }
+
+  })
+
+  return PlayAction
+}
